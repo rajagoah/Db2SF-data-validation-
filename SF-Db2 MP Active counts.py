d4e73ede32e1880cf2cf8d7953296fd5119bfcb4
aakarsh.rajagopalan@gmail.com <Th1$1$mypassword>
2020-02-13 10:56:01 -0500

    1. Coverted the code block into a module specific for MP records processing 2. Added main() to call the SfDb2MPCounts() method 3. In the main() added the input() to prompt user to enter the values of the file paths
    Signed-off-by: aakarsh.rajagopalan@gmail.com <Th1$1$mypassword>

--- SF-Db2 MP Active counts.py
+++ SF-Db2 MP Active counts.py
@@ -2,69 +2,83 @@
 import pandas as pd
 import numpy as np
 
-#setting ipythons max row and column display
-pd.set_option('display.max_row',1000)
-pd.set_option('display.max_column',60)
+def SfDb2MPCounts(db2_path, sf_path):
 
-#storing the db2 data in to a variable
-db2 = pd.read_csv('/Users/aakarsh.rajagopalan/Personal documents/Python projects/CON data set/CON-MP-All statuses 3-13 db2.csv')
-sf = pd.read_csv('/Users/aakarsh.rajagopalan/Personal documents/Python projects/CON data set/CON-MP-All statuses 2-13 sf.csv')
 
-#printing out the top 5 records as a sample.
-print('########## DB2 SHAPE ##########')
-print(db2.shape)
+    #setting ipythons max row and column display
+    pd.set_option('display.max_row',1000)
+    pd.set_option('display.max_column',60)
 
-print('########## DB2 UNIQUE CONTRACT STATUS FIELD ##########')
-print(db2['CONTRACT_STATUS'].unique())
+    #storing the db2 data in to a variable
+    db2 = pd.read_csv(db2_path)
+    sf = pd.read_csv(sf_path)
 
-print('########## SF SHAPE ##########')
-print(sf.shape)
+    #printing out the top 5 records as a sample.
+    print('########## DB2 SHAPE ##########')
+    print(db2.shape)
 
-#converting the entire data frame to string and removing trailing and leading spaces
-#db2_df = db2.columns.str.strip()
-#sf_df = sf.columns.str.strip()
-db2['PROGRAM_TYPE'] = db2['PROGRAM_TYPE'][1:].str.strip()
-db2['PROG_ID'] = db2['PROG_ID'][1:].map(str).str.strip()
-db2['CONTRACT_STATUS'] = db2['CONTRACT_STATUS'][1:].str.strip()
+    print('########## DB2 UNIQUE CONTRACT STATUS FIELD ##########')
+    print(db2['CONTRACT_STATUS'].unique())
 
-#determining the type of object
-print('########## DB2 TYPE ##########')
-print(type(db2))
+    print('########## SF SHAPE ##########')
+    print(sf.shape)
 
-print('########## SF TYPE ##########')
-print(type(sf))
+    #converting the entire data frame to string and removing trailing and leading spaces
+    #db2_df = db2.columns.str.strip()
+    #sf_df = sf.columns.str.strip()
+    db2['PROGRAM_TYPE'] = db2['PROGRAM_TYPE'][1:].str.strip()
+    db2['PROG_ID'] = db2['PROG_ID'][1:].map(str).str.strip()
+    db2['CONTRACT_STATUS'] = db2['CONTRACT_STATUS'][1:].str.strip()
 
-#Db2 cocnatenating the values of the PK fields to create a unique string field
-db2['Unique_string']=db2['PROGRAM_TYPE'].map(str).str.strip()\
-                     +db2['LINE_MAKE'].map(str).str.strip()\
-                     +db2['PROG_ID'].map(str).str.strip()\
-                     +db2['MODEL_CODE'].map(str).str.strip()\
-                     +db2['MODEL_YEAR'].map(str).str.strip()\
-                     +db2['CHASSIS_NBR'].map(str).str.strip()
+    #determining the type of object
+    print('########## DB2 TYPE ##########')
+    print(type(db2))
 
-#SF cocnatenating the values of the PK fields to create a unique string field
-sf['Unique_string']=sf['VCS2_Program_Type__c'].map(str).str.strip()\
-                     +sf['VCS2_Line_Make__c'].map(str).str.strip()\
-                     +sf['VCS2_Program_Id__c'].map(str).str.strip()\
-                     +sf['VCS2_Model_Code__c'].map(str).str.strip()\
-                     +sf['VCS2_Model_Year__c'].map(str).str.strip()\
-                     +sf['VCS2_Chassis_Number__c'].map(str).str.strip()
+    print('########## SF TYPE ##########')
+    print(type(sf))
 
-#filtering a subset to hold only MP Active contracts using chaining
-db2_mp_act = db2.query('CONTRACT_STATUS == "Active"')
-print('number of rows in db2_mp -->', len(db2_mp_act))
+    #Db2 cocnatenating the values of the PK fields to create a unique string field
+    db2['Unique_string']=db2['PROGRAM_TYPE'].map(str).str.strip()\
+                         +db2['LINE_MAKE'].map(str).str.strip()\
+                         +db2['PROG_ID'].map(str).str.strip()\
+                         +db2['MODEL_CODE'].map(str).str.strip()\
+                         +db2['MODEL_YEAR'].map(str).str.strip()\
+                         +db2['CHASSIS_NBR'].map(str).str.strip()
 
-sf_mp_act = sf.query('VCS2_Contract_Status__c == "Active"')
-print('number of rows in sf_mp -->', len(sf_mp_act))
+    #SF cocnatenating the values of the PK fields to create a unique string field
+    sf['Unique_string']=sf['VCS2_Program_Type__c'].map(str).str.strip()\
+                         +sf['VCS2_Line_Make__c'].map(str).str.strip()\
+                         +sf['VCS2_Program_Id__c'].map(str).str.strip()\
+                         +sf['VCS2_Model_Code__c'].map(str).str.strip()\
+                         +sf['VCS2_Model_Year__c'].map(str).str.strip()\
+                         +sf['VCS2_Chassis_Number__c'].map(str).str.strip()
 
-print('difference in count of rows -->', len(sf_mp_act) - len(db2_mp_act))
+    #filtering a subset to hold only MP Active contracts using chaining
+    db2_mp_act = db2.query('CONTRACT_STATUS == "Active"')
+    print('number of rows in db2_mp -->', len(db2_mp_act))
 
-#Creating a third list to store the excess records. Making use of setdiff1d function in numpy module to find out
-# values present in sf_mp_act but not in db2_mp_act
-recs_not_present = np.setdiff1d(sf_mp_act['Unique_string'], db2_mp_act['Unique_string'],assume_unique=False)
+    sf_mp_act = sf.query('VCS2_Contract_Status__c == "Active"')
+    print('number of rows in sf_mp -->', len(sf_mp_act))
 
-print('########## Number of excess records found ##########')
-print(len(recs_not_present))
+    print('difference in count of rows -->', len(sf_mp_act) - len(db2_mp_act))
 
-print('########## The values are ##########')
-print(recs_not_present)
+    #Creating a third list to store the excess records. Making use of setdiff1d function in numpy module to find out
+    # values present in sf_mp_act but not in db2_mp_act
+    recs_not_present = np.setdiff1d(sf_mp_act['Unique_string'], db2_mp_act['Unique_string'],assume_unique=False)
+
+    print('########## Number of excess records found ##########')
+    print(len(recs_not_present))
+
+    return recs_not_present
+
+if __name__=="__main__":
+
+    #taking input from user and directly passing value to the SfDb2MPCounts() module
+    #db2 = '/Users/aakarsh.rajagopalan/Personal documents/Python projects/CON data set/CON-MP-All statuses 3-13 db2.csv'
+    #sf = '/Users/aakarsh.rajagopalan/Personal documents/Python projects/CON data set/CON-MP-All statuses 2-13 sf.csv'
+
+    recs_not_present = SfDb2MPCounts(input('Enter the db2 file path -- '),input('Enter the sf file path -- '))
+
+    print('########## The values are ##########')
+    print(recs_not_present)
+
